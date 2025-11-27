f9cc41baf0247c4c1527bd8c349b201714c693ea
unknown <987omar123osama456@gmail.com>
2025-11-27 15:42:05 +0200

    omar

--- database/migrations/2025_11_27_132602_remove_onesignal_player_id_from_users_table.php
+++ database/migrations/2025_11_27_132602_remove_onesignal_player_id_from_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('onesignal_player_id');
-            $table->dropColumn('device_type');
+            // $table->dropColumn('device_type');
             $table->dropColumn('fcm_token');
             
         });
